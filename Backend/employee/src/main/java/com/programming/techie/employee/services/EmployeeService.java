4e3fd5c1a3aa84bf6ee9e1f50299306592863e05
Adeniyi-Bella <adeniyibella@gmail.com>
2023-04-01 21:34:38 +0200

    interface method to perform operations on main service class

---
+++ Backend/employee/src/main/java/com/programming/techie/employee/services/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.programming.techie.employee.services;
+
+
+import com.programming.techie.employee.model.Employee;
+
+// import java.util.List;
+// interface that contains methods to perform CRUD operations on Employee
+public interface EmployeeService {
+    Employee createEmployee(Employee employee);
+
+   /* List<Employee> getAllEmployees();
+
+    boolean deleteEmployee(Long id);
+
+    Employee getEmployeeById(Long id);
+
+    Employee updateEmployee(Long id, Employee employee);*/
+}

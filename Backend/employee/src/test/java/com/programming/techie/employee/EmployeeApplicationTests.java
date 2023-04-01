202d8546a4034be1f2da647951ee882ba88bae06
Adeniyi-Bella <adeniyibella@gmail.com>
2023-04-01 21:38:45 +0200

    test file renamed

--- Backend/employee/src/test/java/com/programming/techie/employee/EmployeeApplicationTests.java
+++ Backend/employee/src/test/java/com/programming/techie/employee/EmployeeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.programming.Backend;
+package com.programming.techie.employee;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class EmployeeApplicationTests {
 
 	@Test
 	void contextLoads() {

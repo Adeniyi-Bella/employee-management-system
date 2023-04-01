b534689c53a59722578319f7457b24f5d93aecbf
Adeniyi-Bella <adeniyibella@gmail.com>
2023-04-01 21:22:59 +0200

    main class created that runs the application

---
+++ Backend/employee/src/main/java/com/programming/techie/employee/EmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.programming.techie.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeApplication {
+	// Main method to run the application
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeApplication.class, args);
+	}
+
+}

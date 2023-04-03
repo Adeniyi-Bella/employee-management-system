b7ca9c2e2d44a77e853c281f161dad13cbc215e0
Adeniyi-Bella <adeniyibella@gmail.com>
2023-04-04 01:17:04 +0200

    routes path added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import EmployeeList from "./components/EmployeeList";
 import AddEmployee from "./components/AddEmployee";
+import UpdateEmployee from "./components/UpdateEmployee";
+
 
 
 function App() {
@@ -12,9 +14,11 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-
+        {/* route paths added to respond to users actions */}
         <Route path="/" element={<EmployeeList />}></Route>
         <Route path="/addEmployee" element={<AddEmployee />} />
+        <Route path="/editEmployee/:id" element={<UpdateEmployee />} />
+
 
 
         </Routes>
